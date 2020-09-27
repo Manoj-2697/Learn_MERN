9050a593b7261b6e733762696ef395ee1f78838a
ManojKumar <manoj.manojkumar.2697@gmail.com>
2020-09-28 00:38:49 +0530

    Added few lines in server.js file

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ const app = express();
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT | 5001;
 app.listen(PORT, () => console.log({ PORT }));
+console.log('Hello Coder!!');
